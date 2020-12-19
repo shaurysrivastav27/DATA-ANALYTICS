f8b29e42b2f7daa61aef4db2bb6299fe477fed28
SHAURY SRIVASTAV <66357200+shaurysrivastav27@users.noreply.github.com>
2020-12-19 22:34:16 +0530

    Update iris.r

--- iris/iris.r
+++ iris/iris.r
@@ -4,16 +4,16 @@ iris= read.csv("/home/shaury/Downloads/nptel/Iris.csv")
 > test<-subset(iris,sample==FALSE)
 > train <-subset(iris,sample==TRUE)
 > library(DMwR)
-%%	Loading required package: grid
-%%	Registered S3 method overwritten by 'quantmod':
-%%  method            from
-%%  as.zoo.data.frame zoo 
+#	Loading required package: grid
+#	Registered S3 method overwritten by 'quantmod':
+#  method            from
+#  as.zoo.data.frame zoo 
 > nn1 <- kNN(iris$Species~.,train,test,norm=FALSE,k=6)
 > table(test[,6],nn1)
-%%                 nn1
-%%                  Iris-setosa Iris-versicolor Iris-virginica
-%%  Iris-setosa              16               0              0
-%%  Iris-versicolor           0              17              0
-%%  Iris-virginica            0               0             17
+#                 nn1
+#                  Iris-setosa Iris-versicolor Iris-virginica
+#  Iris-setosa              16               0              0
+#  Iris-versicolor           0              17              0
+#  Iris-virginica            0               0             17
 
 
