524b60be1eac33739ccfe97dabd4df0943c2dcfc
SHAURY SRIVASTAV <66357200+shaurysrivastav27@users.noreply.github.com>
2021-01-16 01:57:31 +0530

    Create iris_lda.r

---
+++ iris/iris_lda.r
@@ -0,0 +1,18 @@
+df<- read.csv("/home/shaury/Downloads/nptel/Iris.csv")
+ plot(df,col=c("red","blue","green")[ df$Species])
+ library(MASS)
+ library(tidyverse)
+ set.seed(123)
+ library(caTools)
+ sample<-sample.split(df,SplitRatio=0.8)
+ train<-subset(df,sample==TRUE)
+ test<-subset(df,sample==FALSE)
+ View(train)
+ l1<-lda(train$Species~ train$PetalLengthCm+train$PetalWidthCm+train$SepalWidthCm+train$SepalLengthCm)
+ l1
+ train=test
+ pred<-predict(l1,data.frame(train))
+ table(pred$class,test$Species)
+ score<-mean(pred$class==test$Species)
+ score
+
