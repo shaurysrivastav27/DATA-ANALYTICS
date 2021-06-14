3d0289cd318a144a04338a8908fb62d2a9aade3e
shaurysrivastav27 <shaury.btech.ec19@iiitranchi.ac.in>
2021-06-14 15:13:47 +0530

    Updated titanic.r

---
+++ bonds prediction/readme.md
@@ -0,0 +1,11 @@
+# bonds prediction -NPTEL
+### visuals
+![x vs y](https://github.com/shaurysrivastav27/DATA-ANALYTICS/blob/master/bonds%20prediction/xvsy.png)
+#### after removing the outliers
+![x vs y](https://github.com/shaurysrivastav27/DATA-ANALYTICS/blob/master/bonds%20prediction/xvsy2.png)
+### models
+![model 1](https://github.com/shaurysrivastav27/DATA-ANALYTICS/blob/master/bonds%20prediction/xvsym1.png)
+![residuals for model 1](https://github.com/shaurysrivastav27/DATA-ANALYTICS/blob/master/bonds%20prediction/res.png)
+#### final model
+![model 2](https://github.com/shaurysrivastav27/DATA-ANALYTICS/blob/master/bonds%20prediction/xvsym2.png)
+
--- titanic/titanic.r
+++ titanic/titanic.r
@@ -1,32 +1,36 @@
 
  train<- read.csv("/home/shaury/Downloads/nptel/titanic/train.csv")
  test<- read.csv("/home/shaury/Downloads/nptel/titanic/test.csv")
- View(train)
+ #View(train)
  library(tidyverse)
  train<-na.omit(train)
- ggplot(train)+geom_point( aes(train$Survived,train$PassengerId,col=Pclass))
- ggplot(train)+geom_point( aes(train$Survived,train$PassengerId,col=Sex))
- m1<- glm(train$Survived~ train$Sex+train$Pclass)
- summary(m1)
-#unsupervised learning as test has no survival column
- train= test
- y_pred= predict(m1,train)
+ png("/home/shaury/Desktop/pvsc/pclass.png");
+ pl<-ggplot(train)+geom_bar(aes(Survived,fill=factor(Pclass)))
+ print(pl);
+ dev.off();
+ 
+ png("/home/shaury/Desktop/pvsc/pclass-diff.png");
+ pl<-ggplot(train)+geom_bar(aes(Survived,fill=factor(Pclass)),position='dodge')
+ print(pl)
+ dev.off();
+ 
+ png("/home/shaury/Desktop/pvsc/sex.png");
+ pl<-ggplot(train)+geom_bar(aes(Survived,fill=factor(Sex)))
+ print(pl);
+ dev.off();
+ 
+ png("/home/shaury/Desktop/pvsc/sex-diff.png");
+ pl<-ggplot(train)+geom_bar(aes(Survived,fill=factor(Sex)),position='dodge')
+ print(pl);dev.off();
+ 
+ m1<- glm(Survived~ Sex+Pclass,train,family='gaussian');
+ summary(m1);
+ 
+ pred<- predict(m1,train);
+ pred<- round(pred,0);
+ table(pred,train$Survived);
+ 
+ #test data
+ y_pred= predict(m1,test)
  y_pred=round(y_pred,0)
- View(y_pred)
- train<- read.csv("/home/shaury/Downloads/nptel/titanic/train.csv")
- train=na.omit(train)
- View(train)
- y_pred = predict(m1,train)
- y_pred= round(y_pred,0)
- table(y_pred,train$Survived)
- mean(y_pred==train$Survived)
-#lda analysis starts
- library(class)
- library(MASS)
- m2<-lda(train$Survived~train$Pclass+train$Sex)
- summary(m2)
- y_pred = predict(m2,train)
- View(y_pred)
- table(y_pred$class,train$Survived)
-mean(y_pred==train$Survived)
 
