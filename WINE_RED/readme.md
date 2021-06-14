746a22522e8a4ed5d3a20f92b4aa89eda5630557
shaurysrivastav27 <shaury.btech.ec19@iiitranchi.ac.in>
2021-06-14 14:15:28 +0530

    Updated bonds.r

--- WINE_RED/readme.md
+++ WINE_RED/readme.md
@@ -1,13 +1,17 @@
 # RED WINE QUALITY PREDICTION
 ### components 
 #### visualization 
-![]()
-
+![alcohol vs id](https://github.com/shaurysrivastav27/DATA-ANALYTICS/blob/master/WINE_RED/images/alcohol.png)
+![chlorides vs id](https://github.com/shaurysrivastav27/DATA-ANALYTICS/blob/master/WINE_RED/images/chlorides.png)
+![ph vs id](https://github.com/shaurysrivastav27/DATA-ANALYTICS/blob/master/WINE_RED/images/ph.png)
+![sulphates vs id](https://github.com/shaurysrivastav27/DATA-ANALYTICS/blob/master/WINE_RED/images/sulphates.png)
+![fixed acidity vs id](https://github.com/shaurysrivastav27/DATA-ANALYTICS/blob/master/WINE_RED/images/fixedacid.png)
 #### model 
-##### linear regression
+##### linear discriminant analysis
 ##### knn
 ##### logistic regression
 
  
 #### link to kaggle notebook
 #### https://www.kaggle.com/shaurysrivastava/notebook31cb5e0af8
+
--- bonds prediction/bonds.r
+++ bonds prediction/bonds.r
@@ -1,24 +1,48 @@
-df = read.delim("path/bonds.txt")
+df = read.csv("/home/shaury/Desktop/pvsc/data.sci/DATA-ANALYTICS/bonds prediction/bonds.csv")
+df<-na.omit(df);
+
 x = df$CouponRate
 y = df$BidPrice
-plot(x,y) # comes out to be linear almost
-m1 = lm(y~x) // fit model
-plot(x,y) 
-abline(m1) // plot the fitted line
+
+png("/home/shaury/Desktop/pvsc/data.sci/DATA-ANALYTICS/bonds prediction/xvsy.png");
+print(plot(x,y));
+dev.off();
+
+m1 = lm(y~x) #fit model
+summary(m1);
+
+
+png("/home/shaury/Desktop/pvsc/data.sci/DATA-ANALYTICS/bonds prediction/xvsym1.png");
+pl<-plot(x,y) 
+#plot the fitted line
+print(pl+abline(m1));
+dev.off();
+
 y1 = predict(m1,data.frame(x))
-plot(y1,rstandard(m1)) // plotting residual
-abline(h = 2,lty=2) 
-abline(h= -2,lty=2)
-identify(y1,rstandard(m1)) //identifying point with anamoly behaviour
+
+png("/home/shaury/Desktop/pvsc/data.sci/DATA-ANALYTICS/bonds prediction/res.png");
+pl<-plot(y1,rstandard(m1)) # plotting residual
+print(pl+abline(h = 2,lty=2)+abline(h= -2,lty=2));
+dev.off();
+
+# identify(y1,rstandard(m1)) //identifying point with anamoly behaviour
 # reduced model
-p = c(3,35,4,34) //skewed points
+p = c(3,35,4,34) #skewed points or outliers
 df = df[-c(3,35,4,34),]
 x = df$CouponRate
 y = df$BidPrice
-plot(x,y) # comes out to be linear perfectly
-m1 = lm(y~x) // fit model
-plot(x,y) 
-abline(m1) // plot the fitted line
+png("/home/shaury/Desktop/pvsc/data.sci/DATA-ANALYTICS/bonds prediction/xvsy2.png");
+print(plot(x,y)) # comes out to be linear perfectly
+dev.off();
+
+
+m1 = update(lm(y~x)) # // fit model
+
+png("/home/shaury/Desktop/pvsc/data.sci/DATA-ANALYTICS/bonds prediction/xvsym2.png");
+print(plot(x,y)+abline(m1)) 
+dev.off();
+
+
 y1 = predict(m1,data.frame(x))
 
 summary(m1)
